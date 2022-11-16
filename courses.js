ddb7100f829bbaed719ca55d791f38db1cda0aba
NMLami <nafilmahmud@iut-dhaka.edu>
2022-11-16 13:40:45 +0100

    second commit

---
+++ courses.js
@@ -0,0 +1,16 @@
+var a = [
+    {
+        "continuingOrInitialTraining" : '',
+        "nameOfTheDepartment" : '',
+        "cycleDeFormation" : '',
+        "shortDescriptionOfTheProgramme" : '',
+        "typeOfOrganisation" : '',
+        "nameOfTheOrganisation" : '',
+        "titleOfTheCourse" : '',
+        "location" : '',
+        "priorLearning" : '',
+        "duration" : '',
+        "methodsOfAccess" : '',
+        "url" :''
+    }
+]
